e043252daccbe7dd4a1fd0189070f96fee8347fe
TuShArBhArDwA <tusharbhardwaj2617@gmail.com>
2025-02-16 20:50:19 +0530

    created joblisting component

---
+++ client/src/components/JobListing.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const JobListing = () => {
+  return (
+    <div>
+      {/* Sidebar */}
+      <div>
+        {/* Search Filter from Hero Component*/}
+      </div>
+    </div>
+  )
+}
+
+export default JobListing
