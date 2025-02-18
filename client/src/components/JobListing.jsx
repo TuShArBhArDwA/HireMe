63f9bd6277ec48e315214541dcc105a3d14af001
TuShArBhArDwA <tusharbhardwaj2617@gmail.com>
2025-02-18 15:55:53 +0530

    added pagination

--- client/src/components/JobListing.jsx
+++ client/src/components/JobListing.jsx
@@ -82,17 +82,20 @@ const JobListing = () => {
         </div>
 
         {/* Pagination */}
-        {jobs.length>0 &&(<div>
-          <a href="">
+        {jobs.length>0 &&(<div className='flex items-center justify-center space-x-2 mt-10'>
+          <a href="#job-list">
             <img src={assets.left_arrow_icon} alt="" />
           </a>
           {Array.from({length:Math.ceil(jobs.length/6)}).map((_,index)=>(
             <a href="#job-list">
-                <button>
+                <button className={`w-10 h-10 flex items-center justify-center border border-gray-300 rounded ${currentPage===index+1?'bg-blue-100 text-blue-500':'text-gray-500'}`}>
                   {index+1}
                 </button>
             </a>
           ))}
+          <a href="#job-list">
+            <img src={assets.right_arrow_icon} alt="" />
+          </a>
         </div>)}
       </section>
     </div>
