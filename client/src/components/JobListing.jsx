c32217c65641c87cbeb4b7bfacdf19caa3146374
TuShArBhArDwA <tusharbhardwaj2617@gmail.com>
2025-02-20 19:53:30 +0530

    navigation and pagination

--- client/src/components/JobListing.jsx
+++ client/src/components/JobListing.jsx
@@ -121,11 +121,11 @@ const JobListing = () => {
         </div>
 
         {/* Pagination */}
-        {jobs.length>0 &&(<div className='flex items-center justify-center space-x-2 mt-10'>
+        {filteredJobs.length>0 &&(<div className='flex items-center justify-center space-x-2 mt-10'>
           <a href="#job-list">
             <img onClick={()=>setCurrentPage(Math.max(currentPage-1,1))} src={assets.left_arrow_icon} alt="" />
           </a>
-          {Array.from({length:Math.ceil(jobs.length/6)}).map((_,index)=>(
+          {Array.from({length:Math.ceil(filteredJobs.length/6)}).map((_,index)=>(
             <a href="#job-list">
                 <button onClick={()=>setCurrentPage(index+1)} className={`w-10 h-10 flex items-center justify-center border border-gray-300 rounded ${currentPage===index+1?'bg-blue-100 text-blue-500':'text-gray-500'}`}>
                   {index+1}
@@ -133,7 +133,7 @@ const JobListing = () => {
             </a>
           ))}
           <a href="#job-list">
-            <img onClick={()=>setCurrentPage(Math.min(currentPage+1,Math.ceil(jobs.length/6)))} src={assets.right_arrow_icon} alt="" />
+            <img onClick={()=>setCurrentPage(Math.min(currentPage+1,Math.ceil(filteredJobs.length/6)))} src={assets.right_arrow_icon} alt="" />
           </a>
         </div>)}
       </section>
