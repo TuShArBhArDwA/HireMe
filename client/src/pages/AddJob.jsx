a606b1f2adca37df99de5deffb9e81600e8f94d4
TuShArBhArDwA <tusharbhardwaj2617@gmail.com>
2025-02-22 23:41:12 +0530

    Update AddJob.jsx

--- client/src/pages/AddJob.jsx
+++ client/src/pages/AddJob.jsx
@@ -1,4 +1,6 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import Quill from 'quill';
+import { JobCategories, JobLocations } from '../assets/assets';
 
 const AddJob = () => {
 
@@ -8,6 +10,18 @@ const AddJob = () => {
   const [level,setLevel]=useState('Beginner Level')
   const [salary,setSalary]=useState(0)
 
+  const editorRef=useRef(null)
+  const quillRef=useRef(null)
+
+  useEffect(()=>{
+    // Initiate Quill only once
+    if(!quillRef.current&&editorRef.current){
+      quillRef.current=new Quill(editorRef.current,{
+        theme:'snow',
+      })
+    }
+  },[])
+
   return (
     <form action="">
       <div>
@@ -16,6 +30,39 @@ const AddJob = () => {
         onChange={e=>setTitle(e.target.value)}
         value={title} required
         />
+      </div>
+      <div>
+        <p>Job Description</p>
+        <div ref={editorRef}>
+
+        </div>
+      </div>
+
+      <div>
+        <div>
+          <p>Job Category</p>
+          <select onChange={e=>setCategory(e.target.value)}>
+            {JobCategories.map((category,index)=>(
+              <option key={index} value={category}>{category}</option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <p>Job Location</p>
+          <select onChange={e=>setLocation(e.target.value)}>
+            {JobLocations.map((location,index)=>(
+              <option key={index} value={location}>{location}</option>
+            ))}
+          </select>
+        </div>
+        <div>
+          <p>Job Level</p>
+          <select onChange={e=>setLevel(e.target.value)}>
+              <option value="Beginner level">Beginner level</option>
+              <option value="Intermediate level">Intermediate level</option>
+              <option value="Senior level">Senior level</option>
+            </select>
+        </div>
       </div>
     </form>
   )
