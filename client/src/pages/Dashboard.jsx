06a74073c2d0fcda60e5712c56c178f754157416
TuShArBhArDwA <tusharbhardwaj2617@gmail.com>
2025-02-22 22:36:37 +0530

    Update Dashboard.jsx

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -30,17 +30,17 @@ const Dashboard = () => {
         {/* Left Sidebar with option to add job, manage job, view application */}
         <div className='inline-block min-h-screen border-r-2'>
           <ul className='flex flex-col items-start pt-5 text-gray-800'>
-            <NavLink to={'/dashboard/add-job'}>
+            <NavLink className={({isActive})=>`flex items-center p-3 sm:px-6 gap-2 w-full hover:bg-gray-100 ${isActive&&'bg-blue-100 border-r-4 border-blue-500'}`} to={'/dashboard/add-job'}>
               <img src={assets.add_icon} alt="" />
               <p>Add Job</p>
             </NavLink>
 
-            <NavLink to={'/dashboard/manage-jobs'}>
+            <NavLink className={({isActive})=>`flex items-center p-3 sm:px-6 gap-2 w-full hover:bg-gray-100 ${isActive&&'bg-blue-100 border-r-4 border-blue-500'}`} to={'/dashboard/manage-jobs'}>
               <img src={assets.home_icon} alt="" />
               <p>Manage Jobs</p>
             </NavLink>
 
-            <NavLink to={'/dashboard/view-applications'}>
+            <NavLink className={({isActive})=>`flex items-center p-3 sm:px-6 gap-2 w-full hover:bg-gray-100 ${isActive&&'bg-blue-100 border-r-4 border-blue-500'}`} to={'/dashboard/view-applications'}>
               <img src={assets.person_tick_icon} alt="" />
               <p>View Applications</p>
             </NavLink>
