88f42dcba79434b494f17558b8b89ede7a5d6ddc
TuShArBhArDwA <tusharbhardwaj2617@gmail.com>
2025-02-23 17:04:13 +0530

    setup

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import connectDB from './config/db.js'
 
 // Initialize express
 const app=express()
+
+// Connect to database
+await connectDB()
 
 // Middlewares
 app.use(cors())
