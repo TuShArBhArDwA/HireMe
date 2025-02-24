e4ede160f72a6b50c8b7b5f006b7f9df3612bfa4
TuShArBhArDwA <tusharbhardwaj2617@gmail.com>
2025-02-24 15:24:04 +0530

    added new functions

---
+++ server/routes/companyRoutes.js
@@ -0,0 +1,31 @@
+import express from 'express'
+import { registerCompany,getCompanyData,loginCompany, getCompanyJobApplicants, getCompanyPostedJobs, ChangeJobApplicationStatus, changeVisiblity } from '../controllers/companyController.js'
+
+const router=express.Router()
+
+// Register a company
+router.post('/register',registerCompany)
+
+// Company Login
+router.post('/login',loginCompany)
+
+// Get company data
+router.get('/company',getCompanyData)
+
+// Post a job
+router.post('/post-job',postJob)
+
+// Get Applicants Data of Company
+router.get('/applicants',getCompanyJobApplicants)
+
+// Get company Job List
+router.get('/list-job',getCompanyPostedJobs)
+
+// Change Application Status
+router.post('/change-status',ChangeJobApplicationStatus)
+
+// Change Application Visibility
+router.post('/change-visiblity',changeVisiblity)
+
+
+export default router
