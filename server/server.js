19f4952c8a5d8527e10eed299da969ab451823f3
TuShArBhArDwA <tusharbhardwaj2617@gmail.com>
2025-03-02 17:14:34 +0530

    init

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
-// import './config/instrument.js'
+import './config/instrument.js'
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/db.js'
-// import * as Sentry from "@sentry/node"
-// import { clerkWebhooks } from './controllers/webhooks.js'
+import * as Sentry from "@sentry/node"
+import { clerkWebhooks } from './controllers/webhooks.js'
 // import companyRoutes from './routes/companyRoutes.js'
 // import connectCloudinary from './config/cloudinary.js'
 // import jobRoutes from './routes/jobRoutes.js'
@@ -25,10 +25,10 @@ app.use(express.json())
 
 // Routes
 app.get('/',(req,res)=>res.send("API Working"))
-// app.get("/debug-sentry",function mainHandler(req,res){
-//     throw new Error("My first Sentry error!");
-// })
-// app.post('/webhooks',clerkWebhooks)
+app.get("/debug-sentry",function mainHandler(req,res){
+    throw new Error("My first Sentry error!");
+})
+app.post('/webhooks',clerkWebhooks)
 // app.use('/api/company',companyRoutes)
 // app.use('/api/jobs',jobRoutes)
 // app.use('/api/users',userRoutes)
@@ -36,7 +36,7 @@ app.get('/',(req,res)=>res.send("API Working"))
 // Port
 const PORT= process.env.PORT || 5000
 
-// Sentry.setupExpressErrorHandler(app);
+Sentry.setupExpressErrorHandler(app);
 
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
