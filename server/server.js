f1526168b48480092ca4b9cb9624fdc75b88c2d5
TuShArBhArDwA <tusharbhardwaj2617@gmail.com>
2025-03-02 22:11:05 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ import 'dotenv/config'
 import connectDB from './config/db.js'
 import * as Sentry from "@sentry/node"
 import { clerkWebhooks } from './controllers/webhooks.js'
-import companyRoutes from './routes/companyRoutes.js'
+// import companyRoutes from './routes/companyRoutes.js'
 // import connectCloudinary from './config/cloudinary.js'
 // import jobRoutes from './routes/jobRoutes.js'
 // import userRoutes from './routes/userRoutes.js'
@@ -29,7 +29,7 @@ app.get("/debug-sentry",function mainHandler(req,res){
     throw new Error("My first Sentry error!");
 })
 app.post('/webhooks',clerkWebhooks)
-app.use('/api/company',companyRoutes)
+// app.use('/api/company',companyRoutes)
 // app.use('/api/jobs',jobRoutes)
 // app.use('/api/users',userRoutes)
 
