d177c1f90fe9a4275ea829a4f1f49a684f044eb5
Tushar Bhardwaj <116137083+TuShArBhArDwA@users.noreply.github.com>
2025-03-03 09:42:26 +0530

    Update Footer.jsx

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { assets } from '../assets/assets'
 const Footer = () => {
   return (
     <div className='container px-4 2xl:px-20 mx-auto flex items-center justify-between gap-4 py-3 mt-20'>
-      <img width={160} src={assets.main_logo} alt="" />
+      <img width={160} src={assets.hlogo} alt="" />
       <p className='flex-1 border-l border-gray-400 pl-4 text-sm text-gray-500 max-sm:hidden'>Copyright @TusharBhardwaj | All rights reserved</p>
       <div className='flex gap-2.5'>
         <img width={38} src={assets.facebook_icon} alt="" />
